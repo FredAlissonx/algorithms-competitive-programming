4dbc92145ed6cfd000159ea9cfa42e31ae507de6
Fred Alisson <fredalissonsousa@gmail.com>
2024-01-04 14:32:01 -0300

    update

---
+++ src/main/java/org/algorithms/leetcode/algorithms/easy/MaximumNumberofWordsFoundinSentences_2114.java
@@ -0,0 +1,14 @@
+package org.algorithms.leetcode.algorithms.easy;
+
+public class MaximumNumberofWordsFoundinSentences_2114 {
+    public int mostWordsFound(String[] sentences) {
+        int countWords = 0;
+
+        for(String sentence : sentences){
+            String[] fields = sentence.split(" ");
+            if(fields.length > countWords)
+                countWords = fields.length;
+        }
+        return countWords;
+    }
+}
