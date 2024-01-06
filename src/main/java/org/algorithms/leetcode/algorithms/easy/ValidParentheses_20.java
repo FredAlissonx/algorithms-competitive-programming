eec40621c3d786ea5545d3888476c5d0f048eca3
Fred Alisson <fredalissonsousa@gmail.com>
2024-01-06 16:32:54 -0300

    update

---
+++ src/main/java/org/algorithms/leetcode/algorithms/easy/ValidParentheses_20.java
@@ -0,0 +1,20 @@
+package org.algorithms.leetcode.algorithms.easy;
+
+import java.util.Stack;
+
+public class ValidParentheses_20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
