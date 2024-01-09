caa13df20691575addd35c04ff725af2aeb8fefd
Fred Alisson <fredalissonsousa@gmail.com>
2024-01-09 14:19:38 -0300

    update

---
+++ src/main/java/org/algorithms/leetcode/algorithms/easy/BuildArrayfromPermutation_1920.java
@@ -0,0 +1,13 @@
+package org.algorithms.leetcode.algorithms.easy;
+
+public class BuildArrayfromPermutation_1920 {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
