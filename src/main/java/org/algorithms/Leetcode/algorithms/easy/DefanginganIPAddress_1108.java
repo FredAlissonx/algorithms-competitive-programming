15c713071cc9a96ff24832fba5d6148907d1e2a8
Fred Alisson <fredalissonsousa@gmail.com>
2024-01-23 08:34:55 -0300

    update

---
+++ src/main/java/org/algorithms/Leetcode/algorithms/easy/DefanginganIPAddress_1108.java
@@ -0,0 +1,7 @@
+package org.algorithms.Leetcode.algorithms.easy;
+
+public class DefanginganIPAddress_1108 {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
