c818de5c53d35a1ddc69d1520ccb91cc7e82eedc
Fred Alisson <fredalissonsousa@gmail.com>
2024-07-15 09:58:07 -0300

    new problem

---
+++ src/main/java/org/algorithms/DataLemur/SQL/OddandEvenMeasurements.sql
@@ -0,0 +1,16 @@
+WITH getDay AS (
+  SELECT
+    measurement_time::DATE,
+    measurement_value,
+    ROW_NUMBER() OVER(
+    PARTITION BY measurement_time::DATE
+    ORDER BY measurement_time
+    ) as row_number
+  FROM measurements
+)
+SELECT
+  measurement_time AS measurement_day,
+  SUM(CASE WHEN row_number % 2 != 0 THEN measurement_value END) AS odd_sum,
+  SUM(CASE WHEN row_number % 2 = 0 THEN measurement_value END) AS even_sum
+FROM getDay
+GROUP BY measurement_time;
