4546d20be874b59f10e512fe2374a86381597ce9
Fred Alisson <fredalissonsousa@gmail.com>
2024-08-12 17:31:57 -0300

    new problem

---
+++ src/main/java/org/algorithms/DataLemur/SQL/DepartmentvsCompanySalary.sql
@@ -0,0 +1,18 @@
+WITH compAvg AS (
+  SELECT
+    AVG(amount) AS avg
+  FROM salary
+  WHERE payment_date = '03/31/2024 00:00:00'
+)
+SELECT
+  em.department_id,
+  TO_CHAR(sa.payment_date, 'MM-YYYY') AS payment_date,
+  CASE
+    WHEN AVG(sa.amount) > (SELECT avg FROM compAvg) THEN 'higher'
+    WHEN AVG(sa.amount) < (SELECT avg FROM compAvg) THEN 'lower'
+    ELSE 'same'
+  END AS comparison
+FROM salary AS sa
+INNER JOIN employee AS em ON sa.employee_id = em.employee_id
+WHERE sa.payment_date = '03/31/2024 00:00:00'
+GROUP BY em.department_id, sa.payment_date;
