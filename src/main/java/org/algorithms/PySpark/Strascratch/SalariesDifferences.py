d73ba6b43a706caa9c1b78cfe7c0ddb72f519ca7
Fred Alisson <fredalissonsousa@gmail.com>
2024-08-18 09:42:09 -0300

    new problem

--- src/main/java/org/algorithms/PySpark/Strascratch/SalariesDifferences.py
+++ src/main/java/org/algorithms/PySpark/Strascratch/SalariesDifferences.py
@@ -1,13 +1,13 @@
 # Import your libraries
 from pyspark.sql.functions import col, sum, max
+from pyspark.sql import DataFrame
+from pyspark.sql.column import Column
 
 # Start writing code
-valid_departments_id = [1, 4]
-join_condition = (
-    db_employee["department_id"] == db_dept["id"]
-    )
+valid_departments_id: list[int] = [1, 4]
+join_condition: Column = db_employee["department_id"] == db_dept["id"]
 
-department_salaries = db_employee.join(
+department_salaries: DataFrame = db_employee.join(
         db_dept,
         on=join_condition,
         how="inner"
@@ -20,7 +20,7 @@ department_salaries = db_employee.join(
         max("salary").alias("salary_department")
     )
 
-expression = """
+expression: str = """
     ABS(
         SUM(
         CASE
@@ -30,7 +30,7 @@ expression = """
         )
     )  AS salary_difference
 """
-db_employee = department_salaries.selectExpr(expression)
+db_employee: DataFrame = department_salaries.selectExpr(expression)
 
 # To validate your solution, convert your final pySpark df to a pandas df
 db_employee.toPandas()
