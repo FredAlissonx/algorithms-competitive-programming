b2e65e068d62c52927d15783714269ef004e5b5e
Fred Alisson <fredalissonsousa@gmail.com>
2024-08-25 22:10:05 -0300

    new problem

---
+++ src/main/java/org/algorithms/PySpark/Strascratch/NumberOfUnitsPerNationality.py
@@ -0,0 +1,21 @@
+# Import your libraries
+from pyspark.sql.functions import col, count, desc, lower
+
+# Start writing code
+airbnb_hosts_distinct = airbnb_hosts.filter(col("age") < 30).drop_duplicates()
+
+airbnb_hosts_units =  airbnb_hosts_distinct \
+    .join(
+        airbnb_units,
+        on="host_id",
+        how="inner"
+        ) \
+        .filter(lower(col("unit_type")) == "apartment")
+
+result_df = airbnb_hosts_units \
+    .groupBy("nationality") \
+    .agg(count("*").alias("apartment_count")) \
+    .orderBy(col("apartment_count").desc())
+
+# To validate your solution, convert your final pySpark df to a pandas df
+result_df.toPandas()
