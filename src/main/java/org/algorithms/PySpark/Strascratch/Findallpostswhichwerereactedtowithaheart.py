48c2de03f1f02092d2da8a08b3ba81a188fd154a
Fred Alisson <fredalissonsousa@gmail.com>
2024-09-10 23:21:47 -0300

    new problem

--- src/main/java/org/algorithms/PySpark/Strascratch/Findallpostswhichwerereactedtowithaheart.py
+++ src/main/java/org/algorithms/PySpark/Strascratch/Findallpostswhichwerereactedtowithaheart.py
@@ -4,23 +4,23 @@ from pyspark.sql import functions as F
 
 # Start writing code
 
-facebook_react_post = facebook_reactions.alias("fr").join(
-    facebook_posts.alias("fp"),
+facebook_react_post = facebook_reactions.join(
+    facebook_posts,
     on="post_id",
     how="inner"
+    ) \
+    .select(
+        facebook_posts["*"],
+        facebook_reactions["reaction"]
     )
 
+col_select = [col for col in facebook_posts.columns]
 result_df = facebook_react_post \
     .filter(
         F.col("reaction").contains("heart")
     ) \
-    .select(
-        "fp.post_id",
-        "fp.poster",
-        "fp.post_text",
-        "fp.post_keywords",
-        "fp.post_date"
-    ).distinct()
+    .select(*col_select) \
+    .distinct()
 
 # To validate your solution, convert your final pySpark df to a pandas df
 result_df.toPandas()
