9169b94f8d0e61a3731631a4516d162356881a16
Fred Alisson <fredalissonsousa@gmail.com>
2024-09-29 23:58:30 -0300

    new problem

--- src/main/java/org/algorithms/PySpark/Strascratch/Findallwinerieswhichproducewinesbypossessingaromasofplumcherryrosehazelnut.py
+++ src/main/java/org/algorithms/PySpark/Strascratch/Findallwinerieswhichproducewinesbypossessingaromasofplumcherryrosehazelnut.py
@@ -3,7 +3,7 @@ import pyspark
 from pyspark.sql import functions as F
 
 # Start writing code
-aromas = ["plum", "cherry", "rose", "hazelnut"]
+aromas =["plum", "cherry", "rose", "hazelnut"]
 aromas_pattern = r"\b(" + "|".join(aromas) + r")\b"
 
 result_df =  winemag_p1 \
